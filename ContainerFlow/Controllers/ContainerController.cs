66ccaae1610df4528f9341cecbac6c0d395effd5
Rev Huffman <148085981+Revvvolution@users.noreply.github.com>
2024-10-09 22:38:02 -0400

    Completed implementation and testing for Container API Controller with repository methods.

---
+++ ContainerFlow/Controllers/ContainerController.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ContainerFlow.Models;
+using ContainerFlow.Repositories;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ContainerFlow.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContainerController : ControllerBase
+    {
+        private readonly IContainerRepository _containerRepository;
+
+        private readonly IUserProfileRepository _userProfileRepository;
+
+        public ContainerController(IContainerRepository containerRepository, IUserProfileRepository userProfileRepository)
+        {
+            _containerRepository = containerRepository;
+            _userProfileRepository = userProfileRepository;
+        }
+
+        // GET: api/<ContainerController>
+        [HttpGet("GetAllUserContainers/{id}")]
+        public IActionResult GetUserContainers(int id)
+        {
+            var user = _userProfileRepository.GetUserById(id);
+
+            return user == null ? NotFound() : Ok(_containerRepository.GetAllUserContainers(id));
+        }
+
+        // GET api/<ContainerController>/5
+        [HttpGet("GetContainerById/{id}")]
+        public IActionResult Get(int id)
+        {
+            var container = _containerRepository.GetContainerById(id);
+            if (container == null)
+            {
+                return NotFound();
+            }
+            return Ok(container);
+        }
+
+        // POST api/<ContainerController>
+        [HttpPost]
+        public IActionResult Post([FromBody] Container container)
+        {
+            _containerRepository.Add(container);
+            return CreatedAtAction("Get", new { id = container.Id }, container);
+        }
+
+        // PUT api/<ContainerController>/5
+        [HttpPut("Edit/{id}")]
+        public IActionResult Edit(int id, [FromBody] Container container)
+        {
+            if (id != container.Id)
+            {
+                return BadRequest();
+            }
+            _containerRepository.Update(container);
+            return NoContent();
+        }
+
+        // DELETE api/<ContainerController>/5
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            _containerRepository.Delete(id);
+            return NoContent();
+        }
+    }
+}
---
+++ ContainerFlow/Repositories/IContainerRepository.cs
@@ -0,0 +1,13 @@
+﻿using ContainerFlow.Models;
+
+namespace ContainerFlow.Repositories
+{
+    public interface IContainerRepository
+    {
+        List<Container> GetAllUserContainers(int userId);
+        Container GetContainerById(int id);
+        public void Add(Container container);
+        public void Update(Container container);
+        public void Delete(int containerId);
+    }
+}
